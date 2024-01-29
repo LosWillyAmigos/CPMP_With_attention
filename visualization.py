29172dd085548aa7f57d1b7f62e0797e6177a5ab
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-01-28 22:03:53 -0300

    new file to visualization

---
+++ visualization.py
@@ -0,0 +1,175 @@
+import os
+import numpy as np
+from copy import deepcopy
+from keras.models import load_model
+from CPMP_Layers.Layers import Model_CPMP, LayerExpandOutput, ConcatenationLayer, Reduction
+from CPMP.cpmp_ml import generate_random_layout, get_ann_state, get_move, Layout
+from CPMP import cpmp_ml
+
+ERR_MSG_CYCLE = 'Se ha detectado un ciclo!'
+ERR_MSG_MOVE = 'Se ha intentado realizar un movimiento que no es posible'
+SUCCESS_MSG = 'Problema resuelto!'
+
+def get_ann_state_v2(layout: cpmp_ml.Layout) -> np.ndarray:
+    """
+    The purpose of this function is to prepare the
+    data of a CPMP problem state so that it can be
+    read by a neural network.
+
+    Input: 
+        layout (cpmp.Layout): Current state of the CPMP 
+                              problem.
+    
+    Return:
+        ndarray: matrix with normalized data.
+    """
+    S=len(layout.stacks) # Cantidad de stacks
+    #matriz de stacks
+    b = 2. * np.ones([S,layout.H + 1]) # Matriz normalizada
+    for i,j in enumerate(layout.stacks):
+        b[i][layout.H-len(j) + 1:] = [k/layout.total_elements for k in j]
+        b[i][0] = layout.is_sorted_stack(i)
+    b.shape=(S,(layout.H + 1))
+    return b
+
+def load_cpmp_model(name: str) -> tuple:
+    """
+    The purpose of this function is to load a neural network model 
+    designed to solve the CPMP (Container Pre-Marshalling Problem) 
+    using custom layers such as 'Model_CPMP', 'OutputMultiplication', 
+    'LayerExpandOutput', 'ConcatenationLayer', and 'Reduction'.
+
+    Input: 
+        name (string): The filename that stores the structure of the
+                       model along with its weights.
+
+    Return:
+        tuple: The model and the input size. 
+    """
+    custom_objects = {'Model_CPMP': Model_CPMP,
+                      'LayerExpandOutput': LayerExpandOutput,
+                      'ConcatenationLayer': ConcatenationLayer,
+                      'Reduction': Reduction}
+
+    model = load_model(name, custom_objects= custom_objects)
+
+    return model
+
+def clear_terminal():
+    os.system('cls')
+
+def detect_cycles(states, state):
+    size = len(states)
+
+    for i in range(size):
+        if states[i].stacks == state.stacks: return True
+
+    return False 
+
+def resolve_cpmp_problem(model, stack, height, conteiners):
+    states = []
+    state = generate_random_layout(S= stack, H= height, N= conteiners)
+
+    while(True):
+        states.append(state)
+        if state.unsorted_stacks == 0: break
+
+        nn_state = get_ann_state_v2(state)
+
+        act = model.predict(np.stack([nn_state]), verbose= False)
+        k = np.argmax(act)
+        move = get_move(k, S= stack, H= stack)
+
+        container = state.move(move)
+
+        if container is None:
+            states.append(state)
+            return states, 1, None
+        elif detect_cycles(states, state):
+            states.append(state)
+            return states, 2, move
+
+    return states, 0, None
+
+def show_stack(stack, height):
+    size = len(stack)
+
+    if size != 0: print(f'[{stack[0]:2d}', end= ' ')
+    else: print(f'[{0:2d}', end= ' ')
+
+    for i in range(1, height - 1):
+        if i >= size:
+            print(f'{0:2d}', end= ' ')
+        else:
+            print(f'{stack[i]:2d}', end= ' ')
+
+    if size == height: print(f'{stack[height - 1]:2d}', end= ']')
+    else: print(f'{0:2d}', end=']')
+
+
+def show_matrixs(states, stacks, height):
+    size = len(states)
+
+    for j in range(stacks):
+        for k in range(size):
+            show_stack(states[k].stacks[j], height)
+            print(' ', end= ' ')
+        
+        print('')
+
+def visualize(model, stack, height, conteiners):
+    states, err, move = resolve_cpmp_problem(model, stack, height, conteiners)
+    size = len(states)
+
+    show_matrixs(states, stack, height)
+
+    if err == 1: print(ERR_MSG_CYCLE)
+    elif err == 2: print(f'{ERR_MSG_MOVE}  move: {move}')
+    else: print(SUCCESS_MSG)
+
+def repeat():
+    while(True):
+        print('Desea realizar una nueva prueba? (S/N)')
+        act = input()
+
+        if act.isalpha():
+            if act.lower() == 's': return True
+            if act.lower() == 'n': return False
+        
+        clear_terminal()
+        print('Opción invalida, vuelva a intentar.')
+
+def is_BG_action(self, action):
+    s_o = action[0]; s_d = action[1]
+    if (self.is_sorted_stack(s_o)==False
+    and self.is_sorted_stack(s_d)==True
+    and self.gvalue(s_o) <= self.gvalue(s_d)):
+      return True
+
+    else: return False
+
+def main():
+    file_name = input('Indique el nombre del modelo que desea cargar: ')
+    model  = load_cpmp_model(file_name)
+    
+    if model is None: 
+        print('Modelo no encontrado\n')
+    else:
+        print('Modelo cargado con éxito\n')
+
+    height_size = model.layers[0].input_shape[0][2] - 1
+    stack_size = int(input('Indique la cantidad de stacks: '))
+    print(f'Indique la cantidad de contenedores para problemas {stack_size}x{height_size}:', end= ' ')
+    conteiner_size = int(input())
+
+    while(True):
+        clear_terminal()
+        visualize(model, stack_size, height_size, conteiner_size)
+
+        if not repeat(): break
+
+if __name__ == '__main__':
+    cpmp_ml.get_ann_state = get_ann_state_v2
+    Layout.is_BG_action = is_BG_action
+
+    main()
